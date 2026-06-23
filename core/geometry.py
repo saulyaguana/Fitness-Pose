1e7713aad5a75f48cae6df92269107d48b68c7e9
saulyaguana <saulyaguanacruz@gmail.com>
2026-06-23 11:35:53 -0500

    compute_angle function in the geometry.py file

---
+++ core/geometry.py
@@ -0,0 +1,10 @@
+import math
+import numpy as np
+
+def compute_angle(v1, v2):
+    # Unit vector
+    v1u = v1 / np.linalg.norm(v1)
+    # Unit vector
+    v2u = v2 / np.linalg.norm(v2)
+    # Compute the angle between the two unit vectors
+    angle_deg = np.arccos(np.dot(v1u, v2u)) * 180 / math.pi
