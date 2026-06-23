141ede2e396d52a33ee8b2d43ae07d9e6866236f
saulyaguana <saulyaguanacruz@gmail.com>
2026-06-23 17:46:21 -0500

    Function to connect frame, mediapipe and BodyLandmarks

---
+++ vision/frame_processor.py
@@ -0,0 +1,13 @@
+from vision.camera import read_frame
+from vision.pose_detector import detect_pose, create_pose_detector
+
+def stream_body_landmarks(path):
+    pose = create_pose_detector()
+
+    for frame in read_frame(path):
+        body = detect_pose(frame, pose)
+
+        if body is None:
+            continue
+
+    yield body
